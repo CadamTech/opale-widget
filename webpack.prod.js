f1a15dfab63520259ef5d18e2275ba52134c6d07
Alban Dumouilla <alban@selecteev.io>
2023-10-03 18:17:16 +0200

    Obfuscate

---
+++ webpack.prod.js
@@ -0,0 +1,22 @@
+const TerserPlugin = require('terser-webpack-plugin');
+const WebpackObfuscator = require('webpack-obfuscator'); // Import webpack-obfuscator
+
+module.exports = {
+  entry: './src/modal.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist',
+  },
+  optimization: {
+    minimizer: [new TerserPlugin()],
+  },
+  plugins: [
+    // Add the WebpackObfuscator plugin to obfuscate your code
+    new WebpackObfuscator({
+      rotateStringArray: true,
+      stringArrayThreshold: 0.75,
+    }),
+  ],
+  watch: true,
+  mode: 'production',
+};
