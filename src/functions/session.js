3ccab9521278200da69c093db225ad7b569bed37
Alban Dumouilla <alban@selecteev.io>
2023-10-09 18:55:01 +0200

    Set cookies

--- src/functions/session.js
+++ src/functions/session.js
@@ -1,12 +1,21 @@
 export  function isOver18() {
+  console.log('CHECKING COOKIE');
   var cookies = document.cookie.split(';');
+  console.log(cookies)
   for (var i = 0; i < cookies.length; i++) {
     var cookie = cookies[i].trim();
     if (cookie.startsWith('over_18=')) {
+      console.log('COOKIE FOUND');
       return true; // The "over_18" cookie is set
     }
   }
   return false; // The "over_18" cookie is not set
+}
+
+export function setIsOver18() {
+  console.log('SETTING COOKIE');
+  // set cookie for 1 week
+  document.cookie = 'over_18=true;path=/;max-age=604800;Secure';
 }
 
 // uuid management
--- src/modal.js
+++ src/modal.js
@@ -1,11 +1,20 @@
 import { createModal, openModal } from './functions/modal.js';
 import { loadCSS } from './functions/css.js';
 import { env } from './env.js';
+import { isOver18, setIsOver18 } from './functions/session.js';
 
 // Check if the cookie exists, if not, create and display the initial modal
 window.onload = function () {
-    createModal(); 
-    openModal();
+
+    // If param has ?over18=true, set the cookie 
+    if (window.location.search.includes('over18=true')) {
+      setIsOver18();
+    } else {
+      if (!isOver18()) {
+        createModal(); 
+        openModal();
+      }
+    }
     // Load Milligram CSS
     loadCSS(env.cssFrameWorkUrl);
 };
