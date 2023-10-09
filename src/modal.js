ab2b866af285e6cd55a71654b43993fb94be7951
Alban Dumouilla <alban@selecteev.io>
2023-10-09 20:33:39 +0200

    Remove = true

--- src/modal.js
+++ src/modal.js
@@ -7,7 +7,7 @@ import { isOver18, setIsOver18, checkSignature } from './functions/session.js';
 window.onload = function () {
 
     // If param has ?over18=true, set the cookie 
-    if (window.location.search.includes('over_18=true')) {
+    if (window.location.search.includes('over_18=')) {
       // get value of over_18 param
       const urlParams = new URLSearchParams(window.location.search);
       const over18Param = urlParams.get('over_18');
