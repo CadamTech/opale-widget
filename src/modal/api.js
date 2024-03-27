0562659b8525e5850fd01854f104501c704e5a85
Hamza Zbili <hamza.zbili.92@gmail.com>
2024-03-27 17:21:45 +0100

    built for prod

--- src/modal/api.js
+++ src/modal/api.js
@@ -16,7 +16,7 @@ export async function authRedirect(sessionUUID) {
   const origin = window.location.origin;
   window.location.href = `${
     env.authenticatorURL
-  }/?sessionUUID=${sessionUUID}&origin=${encodeURIComponent(
+  }/?OPALE_SESSION_UUID=${sessionUUID}&origin=${encodeURIComponent(
     origin
   )}&OPALE_WEBSITE_ID=${OPALE_WEBSITE_ID}&OPALE_LANGUAGE=${OPALE_LANGUAGE}&OPALE_THEME=${OPALE_THEME}&OPALE_PRIMARY_COLOR=${encodeURIComponent(
     OPALE_PRIMARY_COLOR
